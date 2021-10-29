6256334297560748d9623b4e06b4328da0871adf
Benjamin van Stein <stein.van2@stud.uni-corvinus.hu>
2021-10-29 10:40:21 +0200

    Chart

--- SOAP/SOAP/Form1.Designer.cs
+++ SOAP/SOAP/Form1.Designer.cs
@@ -29,8 +29,13 @@ namespace SOAP
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chartData = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartData)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -38,18 +43,36 @@ namespace SOAP
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(304, 300);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // chartData
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartData.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartData.Legends.Add(legend1);
+            this.chartData.Location = new System.Drawing.Point(310, 0);
+            this.chartData.Name = "chartData";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartData.Series.Add(series1);
+            this.chartData.Size = new System.Drawing.Size(300, 300);
+            this.chartData.TabIndex = 1;
+            this.chartData.Text = "chart1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chartData);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartData)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +80,7 @@ namespace SOAP
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartData;
     }
 }
 
--- SOAP/SOAP/Form1.cs
+++ SOAP/SOAP/Form1.cs
@@ -10,6 +10,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
 
 namespace SOAP
 {
@@ -19,11 +21,29 @@ namespace SOAP
         public Form1()
         {
             InitializeComponent();
-            Consume();
+            string xmlstring = Consume();
+            LoadXML(xmlstring);
             dataGridView1.DataSource = Rates;
+            Charting();
         }
 
-        void Consume()
+        private void Charting()
+        {
+            chartData.DataSource = Rates;
+            var series = chartData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+            var chartArea = chartData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+            var legend = chartData.Legends[0];
+            legend.Enabled = false;
+        }
+
+        string Consume()
         {
             MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
             GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
@@ -32,7 +52,25 @@ namespace SOAP
             request.endDate = "2020-06-30";
             var response = mnbService.GetExchangeRates(request);
             string result = response.GetExchangeRatesResult;
-            File.WriteAllText("export.xml", result);
+            return result;
+        }
+
+        void LoadXML(string input)
+        {
+            XmlDocument xml = new XmlDocument();
+            LoadXML(input);
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                RateData rd = new RateData();
+                rd.Date = DateTime.Parse(item.GetAttribute("date"));
+                XmlElement child = (XmlElement)item.FirstChild;
+                rd.Currency = child.GetAttribute("curr");
+                rd.Value = decimal.Parse(child.InnerText);
+                int unit = int.Parse(child.GetAttribute("unit"));
+                if (unit != 0) rd.Value = rd.Value / unit;
+                Rates.Add(rd);
+            }
+
         }
     }
 }
