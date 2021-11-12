034eb3dfeff3fb0339a2ecd1590b3eb10444ac1a
Benjamin van Stein <stein.van2@stud.uni-corvinus.hu>
2021-11-12 10:54:48 +0100

    Car Class

---
+++ SantaFactory/SantaFactory/Entitties/Car.cs
@@ -0,0 +1,19 @@
+﻿using SantaFactory.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SantaFactory.Entitties
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            var imageFile = Image.FromFile(@"Images\car.png");
+            g.DrawImage(imageFile, 0, 0, Width, Height);
+        }
+    }
+}
