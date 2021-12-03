8930fe34d1fcfd4c5a78fad1bbde320aa174c40e
Benjamin van Stein <stein.van2@stud.uni-corvinus.hu>
2021-12-03 10:24:22 +0100

    Classes, Reading csv

--- MicroSimulation/MicroSimulation/Form1.cs
+++ MicroSimulation/MicroSimulation/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MicroSimulation.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,32 @@ namespace MicroSimulation
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = null;
+        List<BirthProbability> BirthProbability = null;
+        List<DeathProbability> DeathProbability = null;
         public Form1()
         {
             InitializeComponent();
+            Population = GetPopupalation(@"C:\Users\vanst\AppData\Local\Temp\nép-teszt.csv");
+        }
+
+        public List<Person> GetPopupalation(string csvPatch)
+        {
+            List<Person> population = new List<Person>();
+            using (var sr = new StreamReader(csvPatch, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    var p = new Person();
+                    p.BirthYear = int.Parse(line[0]);
+                    p.Gender = (Gender)Enum.Parse(typeof(Gender), line[1]);
+                    p.NbrOfChildren = int.Parse(line[2]);
+                    population.Add(p);
+                }
+            }
+
+            return population;
         }
     }
 }
